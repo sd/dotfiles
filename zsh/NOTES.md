1a9b4b06314822bcc6f49ce075348cbca1bd9181
Sebastian Delmont <sd@notso.net>
2019-03-01 12:19:08 -0500

    custom zsh scripts

---
+++ zsh/NOTES.md
@@ -0,0 +1,6 @@
+```shell
+rm -rf ~/.oh-my-zsh/custom
+ln -Fs `pwd`/zsh/oh-my-zsh-custom ~/.oh-my-zsh/custom
+mkdir -p ~/.oh-my-zsh/custom/themes
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+```
--- zsh/oh-my-zsh-custom/docker.zsh
+++ zsh/oh-my-zsh-custom/docker.zsh
@@ -2,19 +2,19 @@
 # Docker Tricks
 #==============================================================================
 alias d="docker"
-alias dma="docker-machine"
 alias dco="docker-compose"
-alias dcr='docker-compose run $(basename $PWD) "${@}"'
+
+# alias dcr='docker-compose run $(basename $PWD) "${@}"'
 
 # For docker-compose based projects, with a local rakefile at the root level, and per-service directories where you want to invoke tasks
-function lrake() {
-  if [ -f "./Dockerfile" ]; then
-    DIR=$(basename $PWD)
-    echo "rake --rakefile ../Rakefile ${@} DIR=$DIR"
-    pushd .. >/dev/null
-    rake ${@} DIR=$DIR
-    popd >/dev/null
-  else
-    rake ${@}
-  fi
-}
+# function lrake() {
+#   if [ -f "./Dockerfile" ]; then
+#     DIR=$(basename $PWD)
+#     echo "rake --rakefile ../Rakefile ${@} DIR=$DIR"
+#     pushd .. >/dev/null
+#     rake ${@} DIR=$DIR
+#     popd >/dev/null
+#   else
+#     rake ${@}
+#   fi
+# }
