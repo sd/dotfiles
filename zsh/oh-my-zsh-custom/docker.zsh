e0abea0fc7186f1815c677e84188f20cb3835d89
Sebastian Delmont <sd@notso.net>
2019-04-10 16:29:18 -0400

    2019

--- zsh/oh-my-zsh-custom/docker.zsh
+++ zsh/oh-my-zsh-custom/docker.zsh
@@ -3,18 +3,3 @@
 #==============================================================================
 alias d="docker"
 alias dco="docker-compose"
-
-# alias dcr='docker-compose run $(basename $PWD) "${@}"'
-
-# For docker-compose based projects, with a local rakefile at the root level, and per-service directories where you want to invoke tasks
-# function lrake() {
-#   if [ -f "./Dockerfile" ]; then
-#     DIR=$(basename $PWD)
-#     echo "rake --rakefile ../Rakefile ${@} DIR=$DIR"
-#     pushd .. >/dev/null
-#     rake ${@} DIR=$DIR
-#     popd >/dev/null
-#   else
-#     rake ${@}
-#   fi
-# }
